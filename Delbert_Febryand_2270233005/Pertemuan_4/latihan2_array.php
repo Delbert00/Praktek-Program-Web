8ab1d4aaaee2a3906d87d438a1bf6cdaf985c234
Delbert00 <116429099+Delbert00@users.noreply.github.com>
2022-11-19 22:39:14 +0700

    Add files via upload

---
+++ Delbert_Febryand_2270233005/Pertemuan_4/latihan2_array.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Pemrograman PHP dengan Array</title></head>
+<body>
+<?php
+    $nama[]="Delbert";
+    $nama[]="Febryand";
+    $nama[]="Lubis";
+    echo $nama[1].$nama[2].$nama[0];
+    echo "<br>";
+?>
+</body>
+</html>
